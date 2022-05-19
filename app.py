8b6b505b08d4d5260b3adfb6ed0c0baf851dc3ea
Ronei Simoes <55517128+roneisimoes@users.noreply.github.com>
2022-05-19 14:27:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 
 st.write("# Predição de câncer de mama.")
-st.write("## Exemplo com perímetro, área")
+st.write("## Exemplo com perímetro, Estanqueidade")
 
 st.sidebar.write("## Parâmetros")
 perimetro = st.sidebar.slider("perimetro", -2.0, 4.0, -1.3, 0.1)
-area = st.sidebar.slider("area", -1.5, 4.5, 2.0, 0.1)
+estanqueidade = st.sidebar.slider("estanqueidade", -1.5, 4.5, 2.0, 0.1)
 
 with open("objetos.pkl", "rb") as arquivo:
   ss, dtc = pickle.load(arquivo)
 
 estrutura = { 'perimetro': perimetro,
-              'area': area }
+              'estanqueidade': estanqueidade }
 
 df= pd.DataFrame(estrutura, index=[0])
 
@@ -31,10 +31,10 @@ st.write(f"O diagnóstico dessa pessoa é: **{predicao[0]}**")
 
 predicao = dtc.predict_proba(df)
 predicao = pd.DataFrame(predicao)
-predicao.rename({
-    0: 'B',
-    1: 'M'
-}, axis=1, inplace=True)
+#predicao.rename({
+#    0: 'B',
+ #   1: 'M'
+#}, axis=1, inplace=True)
 
 st.write("Probabilidades")
 st.write(predicao)
