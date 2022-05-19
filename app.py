2cf07fb0207cd340628448e710b763a8bcc6d074
Ronei Simoes <55517128+roneisimoes@users.noreply.github.com>
2022-05-19 13:04:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,17 @@ from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 
 st.write("# Predição de câncer de mama.")
-st.write("## Exemplo com raio, perímetro, área, concavidade, junta côncava")
+st.write("## Exemplo com perímetro, área")
 
 st.sidebar.write("## Parâmetros")
-perimetro = st.sidebar.slider("perímetro", 1.0, 4.2, 3.5, 0.1)
-area = st.sidebar.slider("área", 1.0, 6.0, 2.5, 0.1)
-junta_concava = st.sidebar.slider("Junta côncava", 1.7, 2.6, 2.2, 0.1)
+perimetro = st.sidebar.slider("perímetro", 1.7, 4.2, 3.3, 0.1)
+area = st.sidebar.slider("área", 1.0, 6.0, 3.3, 0.1)
 
 with open("objetos.pkl", "rb") as arquivo:
   ss, dtc = pickle.load(arquivo)
 
 estrutura = { 'perímetro': perimetro,
-              'area': area,
-              'junta côncava': junta_concava }
+              'area': area }
 
 df= pd.DataFrame(estrutura, index=[0])
 
@@ -28,7 +26,7 @@ df = ss.transform(df)
 st.write(df)
 
 predicao = dtc.predict(df)
-st.write(f"O diagnóstico dessa pessoa é: **{predicao[0]}**")
+st.write(f"O diagnóstico dessa pessoa é: **{predicao[1]}**")
 #st.write(predicao)
 
 predicao = dtc.predict_proba(df)
